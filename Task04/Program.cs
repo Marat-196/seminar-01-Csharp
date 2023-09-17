4de0abc4cc1919a24ea1b50e2eb489fcfa94a9c5
Marat <zhumataev.marat@bk.ru>
2023-09-17 16:33:01 +0500

    add Task 004

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -7,4 +7,19 @@
 
 Console.WriteLine("Введите трехзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(number % 10);
+
+if number < 0
+{
+    number = number * -1
+}
+
+if (number >= 100 && number <= 999)
+{
+    Console.WriteLine($"{number} -> {number % 10}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
+
+
